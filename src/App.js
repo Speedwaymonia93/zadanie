6c91466dc6c842615b3b983c61ce43cc04f12d2a
Monika Strzalka <monikastrzalka@gmail.com>
2022-05-23 12:31:18 +0200

    Task submission.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { data } from "./data/data";
+import ChecklistItem from "./ChecklistItem";
+import "./style/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [activeItemsCount, setActiveItemsCount] = useState(0);
+  
+    //Looping over data
+    const items = data.map((item, index) => {
+      return (
+          <>
+          <table className="row checklist-item-container">
+        <tbody>
+        <tr key={item.id}>
+            <td className="cchecklist-item-container_item"><ChecklistItem
+          setActiveItemsCount={setActiveItemsCount}
+          checked={item.is_unread}
+        /></td>
+            <td className="checklist-item-container_item id">{item.id}</td>
+            <td className="checklist-item-container_item from">{item.from}</td>
+            <td className="checklist-item-container_item date">{item.sent_date}</td>
+            <td className="checklist-item-container_item">{item.subject}</td>
+            <td className="checklist-item-container_item">{item.snippet}</td>
+        </tr>
+        </tbody>
+       
+          </table>
+          </>
 
-export default App;
+      );
+    });
+  
+    return (
+      <>
+        <h1>Life Checklist</h1>
+        <div>{items}</div>
+        <h3>Selected items:  {activeItemsCount -1} </h3>
+      </>
+    );
+  };
+  
+  export default App;
